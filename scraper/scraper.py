f280f76edc7666d4e1cb7e67ebd349e51e3dcb57
isaakw <isaakw@uvic.ca>
2024-06-14 01:47:56 -0700

    looked at the html

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,7 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # URL of the page we want to scrape
-url = 'vlr.gg/matches/results'
+url = 'https://vlr.gg/matches/results'
 
 # Send a GET request to the URL
 response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.prettify())
