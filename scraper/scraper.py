aa019450a31e73ad837f20fa1fd532d6f75ea90f
isaakw <isaakw@uvic.ca>
2024-06-17 01:07:42 -0700

    added filtering functionality

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,14 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL of the page we want to scrape
-matchesUrl = 'https://vlr.gg/matches/results'
 
-# Send a GET request to the URL
-response = requests.get(matchesUrl)
+for i in range(1, 511):
+    matchesUrl = 'https://vlr.gg/matches/results?page=' + str(i)
+    response = requests.get(matchesUrl)
+    soup = BeautifulSoup(response.text, 'lxml')
+    for link in soup.find_all('a'):
+        #print(link.get('href'))
+        linkstr = str(link.get('href'))
+        if 'champions' in linkstr:
+            print('https://vlr.gg' + link.get('href'))
 
-soup = BeautifulSoup(response.text, 'lxml')
-for link in soup.find_all('a'):
-    print(link.get('href'))
+    print(i)
 
 #print(soup.prettify())
