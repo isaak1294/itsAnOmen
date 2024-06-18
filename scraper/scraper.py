7657f99a6da63d6cf42e4452b7ff8dc994ada71c
isaakw <isaakw@uvic.ca>
2024-06-18 07:12:01 -0700

    generated csv of all VCT games played

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+j = 0
+links = {}
 
 for i in range(1, 511):
     matchesUrl = 'https://vlr.gg/matches/results?page=' + str(i)
@@ -10,9 +12,12 @@ for i in range(1, 511):
     for link in soup.find_all('a'):
         #print(link.get('href'))
         linkstr = str(link.get('href'))
-        if 'champions' in linkstr:
-            print('https://vlr.gg' + link.get('href'))
+        if 'champions-tour' in linkstr:
+            links[j] = 'https://vlr.gg' + link.get('href')
+            j += 1
 
     print(i)
+
+pd.DataFrame({'Links': links}).to_csv('links.csv')
 
 #print(soup.prettify())
