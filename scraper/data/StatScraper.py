744fdb8d4a9e080d4780d470a258ef3fde07cba8
isaakw <isaak.wiebe5@gmail.com>
2024-06-20 23:13:27 -0700

    just printed a single page and am now thinking about how I wan't to store all of the stats.

--- scraper/data/StatScraper.py
+++ scraper/data/StatScraper.py
@@ -4,8 +4,12 @@ import pandas as pd
 
 links = pd.read_csv('links.csv')
 
-for i in links['Links']:
-    response = requests.get(i)
-    soup = BeautifulSoup(response.text, 'lxml')
-    print(soup.prettify())
-    
+##for i in links['Links']:
+##    response = requests.get(i)
+##    soup = BeautifulSoup(response.text, 'lxml')
+##    print(soup.prettify())
+
+page = str(links.loc[9, 'Links'])
+response = requests.get(page)
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup.prettify())
